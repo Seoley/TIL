7503578c74cd259a43fe5c9bf0416092fcd89cf1
Lee Seoley <dltjfdml@naver.com>
2024-03-12 17:36:22 +0900

    파이썬 프로그래밍 스터디

---
+++ programming/python_study/study_thread.py
@@ -0,0 +1,39 @@
+import time
+
+def thread_first():
+    print("the first thread starts")
+    for i in range(5):
+        print(f"thread_1 is running {i}")
+        time.sleep(0.1)
+    print("the first thread ends")
+
+def thread_second():
+    print("the second thread starts")
+    for i in range(5):
+        print(f"thread_2 is running {i}")
+        time.sleep(0.1)
+    print("the second thread ends")
+
+def thread_third():
+    print("the third thread starts")
+    for i in range(5):
+        print(f"thread_3 is running {i}")
+        time.sleep(0.1)
+    print("the third thread ends")
+
+class Timer():
+    def __init__(self):
+        self.start_time = time.time()
+        self.end_time = None
+
+    def end(self):
+        self.end_time = time.time()
+        print(f"time spent {self.end_time - self.start_time}")
+
+timer = Timer()
+
+thread_first()
+thread_second()
+thread_third()
+
+timer.end()
